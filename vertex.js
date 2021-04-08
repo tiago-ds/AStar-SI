bf2207df0b9468d7f84ccc2b9b29db8c7ab02e8f
Guilherme Afonso <guilherme.asp@live.com>
2021-04-07 23:39:41 -0300

    Adiciona suporte a desenho

--- vertex.js
+++ vertex.js
@@ -1,7 +1,18 @@
 class Vertex {
-	constructor(name) {
+	constructor(name, posX, posY) {
 		this.name = name;
 		this.visited = false;
 		this.frontier = null;
+        this.posX = posX;
+        this.posY = posY;
+        this.radius = 30;
 	}
+
+    draw(){
+        stroke(255, 0, 255);
+        strokeWeight(1);
+        circle(this.posX, this.posY, this.radius);
+        textSize(15);
+        text(this.name, this.posX+20, this.posY+20);
+    }
 }
